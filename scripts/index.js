6fbe3d4e9978f6b6acab3e35fe1ac0f133ee60d3
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-23 18:56:14 +0300

    feat: make whole screen drawable

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 const grid = document.querySelector(".grid");
-const resizeButton = document.getElementById("resize-button");
+
+let mousedown = false;
+
+document.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  mousedown = true;
+});
+
+document.addEventListener("mouseup", (e) => {
+  e.preventDefault();
+  mousedown = false;
+});
 
 function getRandomNumber(upper) {
   return Math.floor(Math.random() * (upper + 1));
@@ -9,41 +20,21 @@ function createGrid(size) {
   while(grid.hasChildNodes()) {
     grid.removeChild(grid.firstChild);
   }
-  for(let i = 1; i <= size; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("id", `row-${i}`)
-    row.classList.add("row");
-    for(let j = 1; j <= size; j++) {
-      const square = document.createElement("div");
-      const blackSquare = document.createElement("div");
-      square.setAttribute("id", `square-${(i - 1) * size + j}`)
-      square.classList.add("square");
-      blackSquare.classList.add("black-square");
-      blackSquare.style.backgroundColor = "black";
-      blackSquare.style.opacity = 0;
-      square.appendChild(blackSquare);
-      square.addEventListener("mouseenter", (e) => {
+  grid.style.gridTemplateColumns = `repeat(160, ${size}px)`;
+  grid.style.gridTemplateRows = `repeat(160, ${size}px)`;
+  for(let i = 1; i <= 16000; i++) {
+    const square = document.createElement("div");
+    const blackSquare = document.createElement("div");
+    square.setAttribute("id", `square-${i}`)
+    square.classList.add("square");
+    square.addEventListener("mouseover", (e) => {
+      if(mousedown) {
         e.preventDefault();
-        if(+blackSquare.style.opacity < 1) {
-          square.style.backgroundColor = `rgb(${getRandomNumber(255)}, ${getRandomNumber(255)}, ${getRandomNumber(255)})`;
-          blackSquare.style.opacity = `${+blackSquare.style.opacity + 0.1}`;
-        }
-      })
-
-      row.appendChild(square);
-    }
-    grid.appendChild(row);
+        square.style.backgroundColor = `rgb(${getRandomNumber(255)}, ${getRandomNumber(255)}, ${getRandomNumber(255)})`;
+      }
+    });
+    grid.appendChild(square);
   }
 }
-
-resizeButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  let userInput = parseInt(prompt("Enter a whole number between 1 and 100"));
-  console.log(userInput);
-  while(isNaN(userInput) || userInput < 1 || userInput > 100) {
-    userInput = parseInt(prompt("Enter a whole number between 1 and 100"));
-  }
-  createGrid(parseInt(userInput));
-})
 
 createGrid(16);

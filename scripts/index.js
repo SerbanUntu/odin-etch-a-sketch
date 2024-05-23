5ed5564e51a0d10a94086983e648ec1d8ad1a28d
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-23 17:42:14 +0300

    feat: add custom grid square length
    The grid generation code is now wrapped inside the `createGrid()` helper
    function. A button at the top of the page causes a prompt to resize the
    grid.

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,38 @@
 const grid = document.querySelector(".grid");
-for(let i = 1; i <= 16; i++) {
-  const row = document.createElement("div");
-  row.setAttribute("id", `row-${i}`)
-  row.classList.add("row");
-  for(let j = 1; j <= 16; j++) {
-    const square = document.createElement("div");
-    square.setAttribute("id", `square-${(i - 1) * 16 + j}`)
-    square.classList.add("square");
-    row.appendChild(square);
+const resizeButton = document.getElementById("resize-button");
+
+function createGrid(size) {
+  while(grid.hasChildNodes()) {
+    grid.removeChild(grid.firstChild);
   }
-  grid.appendChild(row);
+  for(let i = 1; i <= size; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("id", `row-${i}`)
+    row.classList.add("row");
+    for(let j = 1; j <= size; j++) {
+      const square = document.createElement("div");
+      square.setAttribute("id", `square-${(i - 1) * size + j}`)
+      square.classList.add("square");
+
+      square.addEventListener("mouseenter", (e) => {
+        e.preventDefault();
+        square.classList.add("hovered");
+      })
+
+      row.appendChild(square);
+    }
+    grid.appendChild(row);
+  }
 }
+
+resizeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let userInput = parseInt(prompt("Enter a whole number between 1 and 100"));
+  console.log(userInput);
+  while(isNaN(userInput) || userInput < 1 || userInput > 100) {
+    userInput = parseInt(prompt("Enter a whole number between 1 and 100"));
+  }
+  createGrid(parseInt(userInput));
+})
+
+createGrid(16);
